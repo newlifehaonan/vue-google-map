ab52a2bb8886b2d2bd33ca8a45ae5aad314a927b
HJW <harry0520520@gmail.com>
2020-03-23 01:17:30 -0500

    rm secret

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,3 @@
 export const CALLBACK_NAME = 'gmapsCallback';
-export const API_KEY = 'AIzaSyDuyIUt7cwoMgdZYJOkryDCBWhekpdCcI4';
+export const API_KEY = '';
 export const ONE_CITY = {lat: -28.024, lng: 140.887}
